6664bb8c756bfbfe59c3de696d251663268eebdb
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2016-03-09 13:25:16 +0100

    tslint

--- src/gallery.ts
+++ src/gallery.ts
@@ -7,107 +7,107 @@ import {AssetsCollection, AssetsModel} from './assets-collection';
 import {UploadButton} from './filebutton';
 import * as utils from 'utilities';
 
-export function template (name:string): ClassDecorator {
-	return function <T extends Function>(target:T) {
-		let t
-		if (!(t = templates[name])) {
-			throw new Error('could not find template: ' + template)
-		}
-		target.prototype.template = t
-	}
+export function template(name: string): ClassDecorator {
+    return function <T extends Function>(target: T) {
+        let t;
+        if (!(t = templates[name])) {
+            throw new Error('could not find template: ' + template)
+        }
+        target.prototype.template = t
+    }
 }
 
-export function attributes(attrs:Object): ClassDecorator {
-	return function <T extends Function>(target:T) {
-		utils.extend(target.prototype, attrs)
-	}
+export function attributes(attrs: Object): ClassDecorator {
+    return function <T extends Function>(target: T) {
+        utils.extend(target.prototype, attrs)
+    }
 }
 
 export interface GalleryViewOptions extends ViewOptions {
-	uploadButton?: boolean
-	collection?: AssetsCollection
-	url?: string
-	removeable?: boolean
+    uploadButton?: boolean
+    collection?: AssetsCollection
+    url?: string
+    removeable?: boolean
 }
 
 @template('gallery')
-@attributes({className:'assets-gallery gallery', tagName:'div',ui:{button:'.upload-button'}})
+@attributes({ className: 'assets-gallery gallery', tagName: 'div', ui: { button: '.upload-button' } })
 export class GalleryView extends LayoutView<HTMLDivElement> {
-	collection: AssetsCollection
-	selected: AssetsModel
-	private _listView: AssetsListView
-	private _preView: AssetsPreview
-	private _uploadButton: UploadButton
+    public collection: AssetsCollection
+    public selected: AssetsModel
+    private _listView: AssetsListView
+    private _preView: AssetsPreview
+    private _uploadButton: UploadButton
 
-	get listView (): AssetsListView {
-		return this._listView
-	}
+    get listView(): AssetsListView {
+        return this._listView
+    }
 
-	get preView (): AssetsPreview {
-		return this._preView
-	}
+    get preView(): AssetsPreview {
+        return this._preView
+    }
 
-	constructor(options:GalleryViewOptions={}) {
-		(<any>options).regions =  {
-			list: '.gallery-list',
-			preview: '.gallery-preview',
-			upload: '.gallery-upload'
-		}
+    constructor(options: GalleryViewOptions = {}) {
+        (<any>options).regions = {
+            list: '.gallery-list',
+            preview: '.gallery-preview',
+            upload: '.gallery-upload'
+        }
 
-		if (!options.url && !options.collection) {
-			throw new Error('either specify url or collection');
-		}
+        if (!options.url && !options.collection) {
+            throw new Error('either specify url or collection');
+        }
 
-		super(options)
+        super(options);
 
-		let collection = options.collection ? options.collection : new AssetsCollection(null,{
-			url: options.url
-		});
+        let collection = options.collection ? options.collection : new AssetsCollection(null, {
+            url: options.url
+        });
 
-		this.collection = collection
+        this.collection = collection
 
-		this._listView = new AssetsListView({
-			collection: collection
-		});
+        this._listView = new AssetsListView({
+            collection: collection
+        });
 
-		this._preView = new AssetsPreview();
+        this._preView = new AssetsPreview();
 
-		this._uploadButton = new UploadButton({
-			autoUpload: true,
-			url: collection.url,
-			maxSize: 1024*1000,
-			//mimeType: 'image/*'
-		});
+        this._uploadButton = new UploadButton({
+            autoUpload: true,
+            url: collection.url,
+            maxSize: 1024 * 1000,
+            //mimeType: 'image/*'
+        });
 
-		this.listenTo(this._listView, 'selected', this._onItemSelect);
-		this.listenTo(this._listView, 'remove', this._onItemRemove)
-		this.listenTo(this._uploadButton, 'upload', this._onItemCreate);
-		//collection.fetch()
-		this.collection = collection
-	}
+        this.listenTo(this._listView, 'selected', this._onItemSelect);
+        this.listenTo(this._listView, 'remove', this._onItemRemove)
+        this.listenTo(this._uploadButton, 'upload', this._onItemCreate);
+        //collection.fetch()
+        this.collection = collection
+    }
 
-	onRender () {
-		this.regions['list'].show(this._listView);
-		this.regions['preview'].show(this._preView)
-		this.regions['upload'].show(this._uploadButton)
+    onRender() {
+        this.regions['list'].show(this._listView);
+        this.regions['preview'].show(this._preView)
+        this.regions['upload'].show(this._uploadButton)
 
-	}
+    }
 
-	private _onItemCreate (asset) {
+    private _onItemCreate(asset) {
 
-		this.collection.create(asset)
-	}
+        this.collection.create(asset)
+    }
 
-	private _onItemSelect ({model}) {
-		if (this._preView.model === model) return;
+    private _onItemSelect({model}) {
+        if (this._preView.model === model) return;
 
-		this._preView.model = model
-		this.selected = model
-	}
+        this._preView.model = model
+        this.selected = model
+    }
 
-	private _onItemRemove ({model}) {
+    private _onItemRemove({model}) {
 
-	}
+    }
 
 
 }
